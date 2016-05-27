d34481eca07a15f0b44a0aa6483f41b73d94852f
merlin <merlin@hsiao.gq>
2016-05-28 07:05:27 +0800

    网站主页   项目框架   首页  注册 登录 用户信息

---
+++ WYJK.HOME/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WYJK.Data;
+using WYJK.Data.IServices;
+using WYJK.Data.ServiceImpl;
+using WYJK.Entity;
+using WYJK.HOME.Models;
+
+namespace WYJK.HOME.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IMemberService _memberService = new MemberService();
+
+        // GET: User
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+
+
+        public ActionResult Info()
+        {
+            return View("Info");
+        }
+
+
+        public ActionResult DoLogin(LoginViewModels model)
+        {
+            if (ModelState.IsValid)
+            {
+                string sql = $"SELECT * FROM Members where MemberName='{model.Email}' and Password='{model.Password}' " ;
+
+                Members users = DbHelper.QuerySingle<Members>(sql);
+
+                if (users!=null)
+                {
+                    return Info();
+                }
+                
+            }
+            ViewBag.ErrorMessage = "用户名或密码输入错误";
+            return Login();
+        }
+    }
+}
---
+++ WYJK.HOME/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WYJK.HOME.MvcApplication" Language="C#" %>
