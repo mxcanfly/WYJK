d3c0f6ec04f29fa408351d4c8eb0cc08d2eb5c7f
merlin <merlin@hsiao.gq>
2016-05-28 21:22:21 +0800

    统一权限验证  注册  部分链接修正

--- WYJK.HOME/Controllers/UserController.cs
+++ WYJK.HOME/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,6 +9,7 @@ using WYJK.Data;
 using WYJK.Data.IServices;
 using WYJK.Data.ServiceImpl;
 using WYJK.Entity;
+using WYJK.Framework.Captcha;
 using WYJK.HOME.Models;
 
 namespace WYJK.HOME.Controllers
@@ -16,34 +19,96 @@ namespace WYJK.HOME.Controllers
         private readonly IMemberService _memberService = new MemberService();
 
         // GET: User
-        public ActionResult Login()
+        public ActionResult Login(LoginViewModel model)
         {
-            return View("Login");
+            if (this.Session["UserInfo"]!=null)
+            {
+                return Redirect("/User/Info");
+            }
+            else
+            {
+                return View("Login", model);
+            }
         }
+
+        public ActionResult Register(RegisterViewModel model)
+        {
+
+            return View(model);
+        }
+
+
 
 
         public ActionResult Info()
         {
+            Members m = (Members)this.Session["UserInfo"];
+
+            string sql = $"SELECT * FROM Members where MemberID=" + m.MemberID; ;
+
+            Members users = DbHelper.QuerySingle<Members>(sql);
             return View("Info");
         }
 
-
-        public ActionResult DoLogin(LoginViewModels model)
+        [HttpPost]
+        public ActionResult DoLogin(LoginViewModel model)
         {
+
             if (ModelState.IsValid)
             {
-                string sql = $"SELECT * FROM Members where MemberName='{model.Email}' and Password='{model.Password}' " ;
-
-                Members users = DbHelper.QuerySingle<Members>(sql);
-
-                if (users!=null)
+                if (model.CheckCode.ToLower().Equals(Session["CheckCode"].ToString().ToLower()))
                 {
-                    return Info();
+                    string sql = $"SELECT * FROM Members where MemberName='{model.Email}' and Password='{model.Password}' ";
+
+                    Members users = DbHelper.QuerySingle<Members>(sql);
+
+                    if (users != null)
+                    {
+                        this.Session["UserInfo"] = users;
+                        return Redirect("/User/Info");
+                    }
+                    else
+                    {
+                        ViewBag.ErrorMessage = "用户名或密码错误";
+                    }
                 }
-                
+                else
+                {
+                    ViewBag.ErrorMessage = "验证码输入错误";
+                }
+
             }
-            ViewBag.ErrorMessage = "用户名或密码输入错误";
-            return Login();
+            return Login(model);
         }
+
+        #region 显示验证码
+        /// <summary>
+        /// 显示验证码
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public FileContentResult Captcha()
+        {
+            CaptchaOptions options = new CaptchaOptions
+            {
+                GaussianDeviation = 0.4,
+                Height = 35,
+                Background = NoiseLevel.Low,
+                Line = NoiseLevel.Low
+            };
+            using (ICapatcha capatch = new FluentCaptcha())
+            {
+                capatch.Options = options;
+                CaptchaResult captchaResult = capatch.DrawBackgroud().DrawLine().DrawText().Atomized().DrawBroder().DrawImage();
+                using (captchaResult)
+                {
+                    MemoryStream ms = new MemoryStream();
+                    captchaResult.Bitmap.Save(ms, ImageFormat.Gif);
+                    Session["CheckCode"] = captchaResult.Text;
+                    return File(ms.ToArray(), "image/gif");
+                }
+            }
+        }
+        #endregion
     }
 }
--- WYJK.HOME/Models/AccountViewModels.cs
+++ WYJK.HOME/Models/AccountViewModels.cs
@@ -6,10 +6,10 @@ using System.Web;
 
 namespace WYJK.HOME.Models
 {
-    public class LoginViewModels
+    public class LoginViewModel 
     {
         [Required]
-        [Display(Name = "电子邮件")]
+        [Display(Name = "用户名")]
         //[EmailAddress]
         public string Email { get; set; }
 
@@ -20,5 +20,29 @@ namespace WYJK.HOME.Models
 
         [Display(Name = "记住我?")]
         public bool RememberMe { get; set; }
+
+
+        [Required]
+        [Display(Name = "验证码")]
+        //[EmailAddress]
+        public string CheckCode { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "用户名")]
+        //[EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "验证码")]
+        //[EmailAddress]
+        public string CheckCode { get; set; }
     }
 }
